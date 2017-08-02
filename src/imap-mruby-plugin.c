de641c7aa3caffe0315f0d6a424368fc535701a3
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2017-08-02 07:42:10 +0000

    Add pre and post mruby hook for capability command

--- src/imap-mruby-plugin.c
+++ src/imap-mruby-plugin.c
@@ -37,6 +37,11 @@ static bool cmd_mruby(struct client_command_context *cmd)
   mrb_state *mrb = imctx->mrb;
   mrbc_context *c = mrbc_context_new(mrb);
 
+  if (mrb == NULL) {
+    i_error("mrb_open() failed.");
+    return FALSE;
+  }
+
   if (!client_read_args(cmd, 0, 0, &args)) {
     return FALSE;
   }
@@ -74,20 +79,56 @@ static void imap_mruby_client_created(struct client **clientp)
   }
 }
 
+static void mruby_command_run_getenv(struct client_command_context *cmd, const char *env)
+{
+  struct client *client = cmd->client;
+  struct imap_mruby_context *imctx = IMAP_MRUBY_IMAP_CONTEXT(client);
+  mrb_state *mrb = imctx->mrb;
+  mrbc_context *c = mrbc_context_new(mrb);
+  mrb_value v;
+  const char *code_str;
+
+  code_str = mail_user_plugin_getenv(cmd->client->user, env);
+
+  if (code_str == NULL) {
+    i_info("mruby %s hook declined", env);
+    return;
+  }
+
+  i_info("%s inline-code: %s", env, code_str);
+
+  v = mrb_load_string_cxt(mrb, code_str, c);
+  mrbc_context_free(mrb, c);
+
+  if (mrb->exc != 0 && (mrb_nil_p(v) || mrb_undef_p(v))) {
+    v = mrb_obj_value(mrb->exc);
+  }
+
+  i_info("run mruby at %s, return value: %s", env, mrb_str_to_cstr(mrb, mrb_inspect(mrb, v)));
+}
+
 static void mruby_command_pre(struct client_command_context *cmd)
 {
-  i_info("mruby pre");
+  if (strcasecmp(cmd->name, "CAPABILITY") == 0) {
+    mruby_command_run_getenv(cmd, "mruby_pre_capability");
+  }
 }
 
 static void mruby_command_post(struct client_command_context *cmd)
 {
-  i_info("mruby post");
+  if (strcasecmp(cmd->name, "CAPABILITY") == 0) {
+    mruby_command_run_getenv(cmd, "mruby_post_capability");
+  }
 }
 
 void imap_mruby_plugin_init(struct module *module)
 {
   i_info("mruby_plugin_init");
+
+  /* add MRUBY command */
   command_register("MRUBY", cmd_mruby, 0);
+
+  //* callback function each command */
   command_hook_register(mruby_command_pre, mruby_command_post);
 
   imap_mruby_module = module;
